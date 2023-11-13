6dcc42b6bd057626e2262bd8582145e840f6619c
al-shaimon <alshaimon968@gmail.com>
2023-11-14 02:37:59 +0600

    Initial setup and navbar

---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+import Camp from '@/app/components/Camp';
+import Features from '@/app/components/Features';
+import GetApp from '@/app/components/GetApp';
+import Guide from '@/app/components/Guide';
+import Hero from '@/app/components/Hero';
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <Camp />
+      <Guide />
+      <Features />
+      <GetApp />
+    </>
+  );
+}
